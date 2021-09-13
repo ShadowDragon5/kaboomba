884a364b3f2c39dae1c300b549326d6e45b3b2bd
pijoneris <benasmiliunas@gmail.com>
2021-09-13 20:21:01 +0300

    Kryonet client-server example

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,32 @@
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String ...args) {
-        System.out.println("Test");
+        Client client = new Client();
+        client.start();
+        client.addListener(new Listener() {
+            public void received (Connection connection, Object object) {
+                if (object instanceof String) {
+                    System.out.println(object);
+                }
+            }
+        });
+
+        try {
+            client.connect(5000, "localhost", 54555);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        client.sendTCP("Here is the request");
+
+        while(true){
+
+        }
     }
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,33 @@
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String ...args){
-        System.out.println("test");
+        Server server = new Server();
+        server.start();
+        try {
+            server.bind(54555);
+        } catch (IOException e) {
+            System.out.println("Unable to start server");
+            e.printStackTrace();
+        }
+
+        server.addListener(new Listener() {
+            public void received (Connection connection, Object object) {
+                System.out.println("Connected" + connection.getID());
+                if (object instanceof String) {
+                    System.out.println(object);
+                    connection.sendTCP("Thanks");
+                }
+            }
+
+            @Override
+            public void disconnected(Connection connection) {
+                System.out.println("Disconnected" + connection.getID());
+            }
+        });
     }
 }
